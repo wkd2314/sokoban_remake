0beec24d73cc1bf6cffd56198b99c13a93a8bb04
somesome <wkddnjsdlr1@gmail.com>
2022-12-10 17:43:00 +0900

    add player movement

--- Assets/Scripts/PlayerTileMovement.cs
+++ Assets/Scripts/PlayerTileMovement.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class PlayerTileMovement : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    public Transform movePoint;
     // Start is called before the first frame update
     void Start()
     {
-
+        movePoint.parent = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // whether has block 
-        // if ()
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        {
+
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+            }
+            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                movePoint.position += new Vector3(0f, 0f, Input.GetAxisRaw("Vertical"));
+            }
+        }
     }
 }
